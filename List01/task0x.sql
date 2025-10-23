bc61086e0ea4fcfcfacae66f2b46aca005c153ec
Alex <146887001+alerog36@users.noreply.github.com>
2025-10-23 20:15:57 +0200

    Finished task 0A

---
+++ List01/task0x.sql
@@ -0,0 +1,53 @@
+--------------------------------------------------------------------------------
+-- TASK 0A
+--------------------------------------------------------------------------------
+CREATE TABLE Bandy (
+    nr_bandy   NUMBER(2)    CONSTRAINT pk_bandy PRIMARY KEY,
+    nazwa      VARCHAR2(20) CONSTRAINT nn_bandy_nazwa NOT NULL,
+    teren      VARCHAR2(15) CONSTRAINT uk_bandy_teren UNIQUE,
+    szef_bandy VARCHAR2(15) CONSTRAINT uk_bandy_szef_bandy UNIQUE
+);
+
+CREATE TABLE Funkcje (
+    funkcja   VARCHAR2(10) CONSTRAINT pk_funkcje PRIMARY KEY,
+    min_myszy NUMBER(3)    CONSTRAINT ck_funkcje_min_myszy CHECK (min_myszy > 5),
+    max_myszy NUMBER(3)    CONSTRAINT ck_funkcje_max_myszy CHECK (max_myszy < 200 AND max_myszy >= min_myszy)
+);
+
+CREATE TABLE Wrogowie (
+    imie_wroga       VARCHAR2(15) CONSTRAINT pk_wrogowie PRIMARY KEY,
+    stopien_wrogosci NUMBER(2)    CONSTRAINT ck_wrogowie_stopien_range CHECK (stopien_wrogosci BETWEEN 1 AND 10),
+    gatunek          VARCHAR2(15),
+    lapowka          VARCHAR2(20)
+);
+
+CREATE TABLE Kocury (
+    imie            VARCHAR2(15) CONSTRAINT nn_kocury_imie NOT NULL,
+    plec            VARCHAR2(1)  CONSTRAINT ck_kocury_plec_md CHECK (plec IN ('M', 'D')),
+    pseudo          VARCHAR2(15) CONSTRAINT pk_kocury PRIMARY KEY,
+    funkcja         VARCHAR2(10) CONSTRAINT fk_funkcje REFERENCES Funkcje(funkcja),
+    szef            VARCHAR2(15) CONSTRAINT fk_kocury_szef REFERENCES Kocury(pseudo),
+    w_stadku_od     DATE DEFAULT SYSDATE,
+    przydzial_myszy NUMBER(3),
+    myszy_extra     NUMBER(3),
+    nr_bandy        NUMBER(2)    CONSTRAINT fk_bandy REFERENCES Bandy(nr_bandy)
+);
+
+CREATE TABLE Wrogowie_kocurow (
+    pseudo         VARCHAR2(15) CONSTRAINT fk_kocury_pseudo REFERENCES Kocury(pseudo),
+    imie_wroga     VARCHAR2(15) CONSTRAINT fk_wrogowie REFERENCES Wrogowie(imie_wroga),
+    data_incydentu DATE         CONSTRAINT nn_wrogowie_kocurow_data NOT NULL,
+    opis_incydentu VARCHAR2(50),
+    CONSTRAINT pk_wrogowie_kocurow PRIMARY KEY (pseudo, imie_wroga)
+);
+
+-- Adding constraint to the szef_bandy field in Bandy table
+ALTER TABLE Bandy 
+ADD CONSTRAINT fk_kocury_szef_bandy FOREIGN KEY (szef_bandy) REFERENCES Kocury(pseudo);
+
+-- Save changes
+COMMIT;
+
+--------------------------------------------------------------------------------
+-- TASK 0B 
+--------------------------------------------------------------------------------
