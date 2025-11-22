5385a96fa121e84ae3ff8fa981fe2e4aee9b559d
Alex <146887001+alerog36@users.noreply.github.com>
2025-11-22 14:54:11 +0100

    Last one task left.

--- List02/Microsoft/microsoft_common_tasks.sql
+++ List02/Microsoft/microsoft_common_tasks.sql
@@ -10,6 +10,9 @@
 --------------------------------------------------------------------------------
 -- TASK 30
 --------------------------------------------------------------------------------
+DROP VIEW Statystyki_band;
+GO
+
 CREATE OR ALTER VIEW Statystyki_band (
     [NAZWA_BANDY],
     [SRE_SPOZ],
@@ -29,6 +32,7 @@ FROM Kocury k
     INNER JOIN Bandy b ON b.nr_bandy = k.nr_bandy
 GROUP BY
     nazwa;
+
 GO
 
 -- Zawartosc perspektywy
@@ -55,7 +59,86 @@ WHERE
 --------------------------------------------------------------------------------
 -- TASK 31
 --------------------------------------------------------------------------------
+GO
 
+CREATE OR ALTER VIEW Najdluzsze_staze (
+    pseudonim,
+    plec,
+    myszy,
+    extra,
+    num_bandy
+) AS
+WITH Koty_z_czarni_rycerze AS (
+    SELECT TOP(3)
+        k.pseudo
+    FROM Kocury k
+        INNER JOIN Bandy b ON b.nr_bandy = k.nr_bandy
+    WHERE
+        b.nazwa = 'CZARNI RYCERZE'
+    ORDER BY
+        k.w_stadku_od
+),
+Koty_z_laciaci_mysliwi AS (
+    SELECT TOP(3)
+        k.pseudo
+    FROM Kocury k
+        INNER JOIN Bandy b ON b.nr_bandy = k.nr_bandy
+    WHERE
+        b.nazwa = 'LACIACI MYSLIWI'
+    ORDER BY
+        k.w_stadku_od
+)
+SELECT
+    k.pseudo,
+    k.plec,
+    k.przydzial_myszy,
+    k.myszy_extra,
+    k.nr_bandy
+FROM Kocury k
+    LEFT JOIN Koty_z_czarni_rycerze cr ON cr.pseudo = k.pseudo
+    LEFT JOIN Koty_z_laciaci_mysliwi lm ON lm.pseudo = k.pseudo
+WHERE
+    cr.pseudo IS NOT NULL
+    OR
+    lm.pseudo IS NOT NULL;
+
+GO
+
+-- Przed podwyzkami
+SELECT
+    pseudonim        [Pseudonim],
+    plec             [Plec],
+    myszy            [Myszy przed podw.],
+    ISNULL(extra, 0) [Extra przed podw.]
+FROM Najdluzsze_staze;
+
+-- Podwyzka
+BEGIN TRANSACTION;
+
+UPDATE Najdluzsze_staze
+SET
+    myszy = myszy + 
+    CASE 
+        WHEN plec = 'D' THEN 0.1 * (SELECT MIN(przydzial_myszy) FROM Kocury) 
+        WHEN plec = 'M' THEN 10  
+    END,
+    extra = ISNULL(extra, 0) + 0.15 * (
+        SELECT 
+            AVG(ISNULL(k.myszy_extra, 0)) 
+        FROM Kocury k
+        WHERE
+            k.nr_bandy = num_bandy
+    );
+
+-- Po podwyzce
+SELECT
+    pseudonim        [Pseudonim],
+    plec             [Plec],
+    myszy            [Myszy po podw.],
+    ISNULL(extra, 0) [Extra po podw.]
+FROM Najdluzsze_staze;
+
+ROLLBACK;
 
 --------------------------------------------------------------------------------
 -- TASK 32
